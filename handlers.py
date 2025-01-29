176d268fc91d4b7074a675f1dc7a7262a17ac770
Kirill Yudaykin <59344843+BAEstudent@users.noreply.github.com>
2025-01-29 21:34:18 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -10,15 +10,15 @@ from aiogram.types import (
     ReplyKeyboardRemove,
 )
 from typing import Any, Dict
-from states import UserProfile, WaterLogger
+from states import UserProfile, WaterLogger, FoodLogger, ActivityLogger
 from datetime import datetime
 import aiohttp
 from config import WEATHER_TOKEN, CITY_TOKEN
 
 router = Router()
 
-#  Класс для логирования воды
-class WaterLoggerClass():
+# Класс для логирования воды
+class WaterLoggerClass:
     def __init__(self):
         self.daily_water_norm = None
         self.water_consumption = 0
@@ -32,11 +32,41 @@ class WaterLoggerClass():
 water_logger = WaterLoggerClass()
 
 
+# Класс для логирования еды
+class FoodLoggerClass:
+    def __init__(self):
+        self.daily_food_norm = None
+        self.food_consumption = 0
+    
+    def update_food_consumption(self, food_volume: float):
+        self.food_consumption = self.food_consumption + food_volume
+
+    def update_food_norm(self, food_norm: float):
+        self.daily_food_norm = food_norm
+
+food_logger = FoodLoggerClass()
+
+
+# Класс для логирования активности
+class ActivityLoggerClass:
+    def __init__(self):
+        self.activity = 0
+        self.activity_types = {
+            "бег": 10,
+            "зарядка":5
+        }
+    
+    def update_activity(self, activity_type: str, activity_minutes: float):
+        self.activity = self.activity + self.activity_types[activity_type]*activity_minutes
+
+activity_logger = ActivityLoggerClass()
+
+
 @router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
     await state.set_state(UserProfile.weight)
     await message.answer(
-        "Привет! Я бот для трекинга здоровья.\nВведите Ваш вес:",
+        "Привет! Я бот для трекинга здоровья.\nВведите Ваш вес (в кг):",
         reply_markup=ReplyKeyboardRemove(),
     )
 
@@ -61,7 +91,7 @@ async def process_age(message: Message, state: FSMContext) -> None:
     await state.update_data(age=message.text)
     await state.set_state(UserProfile.activity_level)
     await message.answer(
-        f"Введите Ваш уровень активности:",
+        f"Введите Ваш уровень активности в минутах в день:",
     )
 
 @router.message(UserProfile.activity_level)
@@ -69,7 +99,7 @@ async def process_activity_level(message: Message, state: FSMContext) -> None:
     await state.update_data(activity_level=message.text)
     await state.set_state(UserProfile.city)
     await message.answer(
-        f"Введите Ваш город:",
+        f"Введите Ваш город (латиницей):",
     )
 
 @router.message(UserProfile.city)
@@ -136,40 +166,149 @@ async def calculate_daily_water(message: Message, state: FSMContext) -> None:
         f"""Ваша дневная норма воды: {water_norm} """
     )   
 
-    
+
 # Функция для логирования воды
 @router.message(Command("log_water"))
-async def calculate_daily_water(message: Message, state: FSMContext) -> None:
-    await state.set_state(WaterLogger.log_water)
-    await message.answer(
-        "Чтобы залогировать воду, введите объем выпитой воды в мл.:",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@router.message(WaterLogger.log_water)
-async def calculate_daily_water(message: Message, state: FSMContext) -> None:
+async def log_water(message: Message, state: FSMContext) -> None:
     if water_logger.daily_water_norm == None:
         await message.answer(
             """Норма воды не установлена. Пожалуйста, установите норму воды командой /calculate_daily_water"""
         )
         await state.set_state(state=None)
     else:
-        await state.update_data(log_water=message.text)
-        data = await state.get_data()
-        water_volume = int(data['log_water'])
-        water_logger.update_water_consumption(water_volume=water_volume)
+        await state.set_state(WaterLogger.log_water)
+        await message.answer(
+            "Чтобы залогировать воду, введите объем выпитой воды в мл.:",
+            reply_markup=ReplyKeyboardRemove(),
+        )
 
-        if water_logger.daily_water_norm <= water_logger.water_consumption:
-            await message.answer(
-                """Вода залогирована, спасибо! Ваша цель по воде выполнена!""",
-                reply_markup=ReplyKeyboardRemove(),
-            )
-            await state.set_state(state=None)
-        else:
-            water_diff = water_logger.daily_water_norm - water_logger.water_consumption
-            await message.answer(
-                f"""Вода залогирована, спасибо! До выполнения цели по воде осталось: {water_diff} мл""",
-                reply_markup=ReplyKeyboardRemove(),
-            )
-            await state.set_state(state=None)
+
+@router.message(WaterLogger.log_water)
+async def calculate_daily_water(message: Message, state: FSMContext) -> None:
+    await state.update_data(log_water=message.text)
+    data = await state.get_data()
+    await state.set_state(state=None)
+    water_volume = int(data['log_water'])
+    water_logger.update_water_consumption(water_volume=water_volume)
+
+    if water_logger.daily_water_norm <= water_logger.water_consumption:
+        await message.answer(
+            """Вода залогирована, спасибо! Ваша цель по воде выполнена!""",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+    else:
+        water_diff = water_logger.daily_water_norm - water_logger.water_consumption
+        await message.answer(
+            f"""Вода залогирована, спасибо! До выполнения цели по воде осталось: {water_diff} мл""",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+
+
+
+# Функция для расчета нормы еды
+@router.message(Command("calculate_daily_food"))
+async def calculate_daily_food(message: Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    weight = int(data.get('weight'))
+    height = int(data.get('height'))
+    age = int(data.get('age'))
+    activity_level = float(data.get('activity_level'))
+    calories_goal = int(data.get('calories_goal'))
+
+    food_norm = weight*10 + 6.25*height - 5*age + activity_level*10
+    food_logger.update_food_norm(food_norm=food_norm)
+    await message.answer(
+        f"""Ваша дневная норма по калориям: {food_norm} """
+    )   
+
+
+# Функция для логирования еды
+@router.message(Command("log_food"))
+async def log_food(message: Message, state: FSMContext) -> None:
+    if food_logger.daily_food_norm == None:
+        await message.answer(
+            """Норма калорий не установлена. Пожалуйста, установите норму калорий командой /calculate_daily_food"""
+        )
+        await state.set_state(state=None)
+    else:
+        await state.set_state(FoodLogger.log_food)
+        await message.answer(
+            "Чтобы залогировать еды, введите съеденный продукт:",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+
+@router.message(FoodLogger.log_food)
+async def log_food_data(message: Message, state: FSMContext) -> None:
+    await state.update_data(log_food=message.text)
+    await state.set_state(state=None)
+    data = await state.get_data()
+
+    await state.set_state(state=None)
+    # Достаем калории по еде из сообщения
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://world.openfoodfacts.org/cgi/search.pl?action=process&search_terms={data}&json=true") as response:
+            status = response.status
+            if status == 200:
+                result = await response.json()
+                if result['products']:
+                    food_volume = result['products'][0]['nutriments']['energy_value']
+                else:
+                    await message.answer(
+                    """К сожалению, не удалось залогировать еду, продукт не найден""",
+                    reply_markup=ReplyKeyboardRemove()
+                    )
+                    return
+            else:
+                print(f"Ошибка: {result.status_code}")
+                await message.answer(
+                    """К сожалению, не удалось залогировать еду, возникла ошибка""",
+                    reply_markup=ReplyKeyboardRemove()
+                )
+                return
+
+    
+    food_logger.update_food_consumption(food_volume=food_volume)
+
+    if food_logger.daily_food_norm <= food_logger.food_consumption:
+        await message.answer(
+            """Еда залогирована, спасибо! Ваша цель по калориям выполнена!""",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        await state.set_state(state=None)
+    else:
+        food_diff = food_logger.daily_food_norm - food_logger.food_consumption
+        await message.answer(
+            f"""Еда залогирована, спасибо! До выполнения цели по калориям осталось: {food_diff} мл""",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        await state.set_state(state=None)
+
+
+
+# Функция для логирования активности
+@router.message(Command("log_activity"))
+async def log_activity(message: Message, state: FSMContext) -> None:
+    await state.set_state(ActivityLogger.log_activity)
+    await message.answer(
+        "Чтобы залогировать еды, введите съеденный продукт:",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+@router.message(ActivityLogger.log_activity)
+async def log_activity_data(message: Message, state: FSMContext) -> None:
+    await state.update_data(log_activity=message.text)
+    await state.set_state(state=None)
+    data = await state.get_data()
+    data = data.split()
+
+    await state.set_state(state=None)
+    activity_type = data[0]
+    activity_minutes = data[1]
+
+    activity_logger.update_activity(activity_type=activity_type, activity_minutes=activity_minutes)
+    water_logger.update_water_consumption(water_volume=(-activity_minutes*200))
+
+    await message.answer(
+        f"""Активность залогирована, спасибо!/nДополнительно: выпейте {activity_minutes*200} мл воды.""",
+        reply_markup=ReplyKeyboardRemove(),
+    )
